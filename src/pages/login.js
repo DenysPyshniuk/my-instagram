23a8de1e22a71104eeedea365e206f285acfefd4
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-21 11:14:30 -0700

    Fix style for login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -24,7 +24,7 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="Iphone with" />
       </div>
       <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img
               src="/images/logo.png"
@@ -52,14 +52,14 @@ export default function Login() {
             <button
               disabled={isInvalid}
               type="submit"
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
           ${isInvalid && "opacity-50"}`}
             >
               Log In
             </button>
           </form>
         </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
           <p className="text-sm">
             Don't have an account?{` `}
             <Link to="/signup" className="font-bold text-blue-medium">
