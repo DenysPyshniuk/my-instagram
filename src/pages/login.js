a4ab0affb5722bfc12a7bd40e1a5eebc6a280e70
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-21 10:42:25 -0700

    Add Logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 
 export default function Login() {
@@ -24,8 +24,20 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="Iphone with" />
       </div>
       <div className="flex flex-col w-2/5">
-        <p>I will be the form!</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+        </div>
       </div>
     </div>
   );
 }
+
+//text-red-primary
+//text-gray-base
+//border-gray-primary
