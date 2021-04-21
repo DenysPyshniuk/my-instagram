c79f4e6872dd491c806fbf462e7f001e09451938
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-20 18:46:16 -0700

    Set up a login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,26 @@
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
 export default function Login() {
-  return <p>I am the login page</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAdress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAdress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login -Instagram";
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>I am a login page</p>
+    </div>
+  );
 }
