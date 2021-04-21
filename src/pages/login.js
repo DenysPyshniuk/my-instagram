2bef82afc119aafdf2a5a47efa34a79f4817da10
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-21 10:47:48 -0700

    Add input for email address

--- src/pages/login.js
+++ src/pages/login.js
@@ -33,6 +33,16 @@ export default function Login() {
             />
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+          </form>
         </div>
       </div>
     </div>
