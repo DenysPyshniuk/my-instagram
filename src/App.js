7f701dff897cd036e12220be76ce7113abf9375f
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-21 16:03:18 -0700

    Add Not Found route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
