8c54d173d49c97c8607c06c5ffa80516164a437e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-20 19:03:22 -0700

    Add picture to the login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -15,12 +15,17 @@ export default function Login() {
   const handleLogin = () => {};
 
   useEffect(() => {
-    document.title = "Login -Instagram";
+    document.title = "Login - Instagram";
   }, []);
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>I am a login page</p>
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="Iphone with" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form!</p>
+      </div>
     </div>
   );
 }
