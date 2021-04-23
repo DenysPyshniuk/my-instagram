fcfb16f602236aa3959c6f5db04bb16b40671930
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-22 17:01:09 -0700

    Add route for the dashboard page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
