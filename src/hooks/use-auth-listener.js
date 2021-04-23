28e40a3a2c496aa7d5ad051d3d380b122eaae89a
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-23 12:05:04 -0700

    Fix problem with missing setUser

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import FirebaseContext from "../context/firebase";
 
 export default function useAuthListener() {
-  const { user, setUser } = setState(
+  const { user, setUser } = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
   const { firebase } = useContext(FirebaseContext);
@@ -12,6 +12,7 @@ export default function useAuthListener() {
       if (authUser) {
         //we have a user...therefo we can store the user in localstorage
         localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
       } else {
         //we don't have an authUser, therefore clear the localstorage
         localStorage.removeItem("authUser");
