fa6633e3939e4fb2c06e7441b256362064ea8793
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-23 13:23:48 -0700

    Fix syntax

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import FirebaseContext from "../context/firebase";
 
 export default function useAuthListener() {
-  const { user, setUser } = useState(
+  const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
   const { firebase } = useContext(FirebaseContext);
