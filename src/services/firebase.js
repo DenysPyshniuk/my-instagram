7922d59b4f96666b858771e6cf7db6cc7641534c
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-24 17:27:21 -0700

    Implement function getUserByUserId

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,19 @@ export async function doesUsernameExist(username) {
 
   return result.docs.map((user) => user.data().length > 0);
 }
+
+//get user from the firestore where userId === userId (passed from the auth)
+export async function getUserByUserId(userId) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("userId", "==", userId)
+    .get();
+
+  const user = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return user;
+}
