bbbc37a269c1fd0f398a523f3cf8b5851c6f9673
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-24 18:18:49 -0700

    Use propTypes for username and fullname

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,12 @@
-export default function User() {
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+
+export default function User({ username, fullName }) {
   return <p>I'm a user</p>;
 }
+
+User.PropTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string,
+};
