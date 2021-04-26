73d03ef5a6addbebed8badc978cbc66e4193b576
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-25 23:22:03 -0700

    Use Skeleton to show the image and username

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,12 +1,30 @@
+import { memo } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 
-export default function User({ username, fullName }) {
-  return <p>I'm a user</p>;
-}
+const User = ({ username, fullName }) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link
+      to={"/p/${username}"}
+      className="grid grid-cols-4 gap-4 mb-6 items-center"
+    >
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+        />
+      </div>
+      <p>{username}</p>
+    </Link>
+  );
+
+export default User;
 
 User.propTypes = {
   username: PropTypes.string,
-  fullName: PropTypes.string,
+  fullName: PropTypes.string.isRequired,
 };
