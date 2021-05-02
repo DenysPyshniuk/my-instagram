471973caf84c49a4c5ce95042fba450f4ede763a
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-01 22:43:56 -0700

    Fix path

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -7,7 +7,7 @@ export default function User({ username, fullName }) {
     <Skeleton count={1} height={61} />
   ) : (
     <Link
-      to={"/p/${username}"}
+      to={`/p/${username}`}
       className="grid grid-cols-4 gap-4 mb-6 items-center"
     >
       <div className="flex items-center justify-between col-span-1">
@@ -29,5 +29,3 @@ User.propTypes = {
   username: PropTypes.string,
   fullName: PropTypes.string,
 };
-
-User.whyDidYouRender = true;
