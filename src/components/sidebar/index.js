e721f27d58f13617a704a5bb98e70d68ae5ba19a
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-01 22:47:11 -0700

    Add following to the useState

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,16 @@
-import React from "react";
 import useUser from "../../hooks/use-user";
 import User from "./user";
 import Suggestions from "./suggestions";
 
 export default function Sidebar() {
   const {
-    user: { fullName, username, userId },
+    user: { fullName, username, userId, following },
   } = useUser();
 
   return (
     <div className="p-4">
       <User username={username} fullName={fullName} />
-      <Suggestions userId={userId} />
+      <Suggestions userId={userId} following={following} />
     </div>
   );
 }
