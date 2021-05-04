be6d98d0d1ae14269561a9fc75b152a2713803f5
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-03 21:14:35 -0700

    Delete not used useHistory

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import UserContext from "../context/user";
 import * as ROUTES from "../constants/routes";
