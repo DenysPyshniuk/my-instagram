930bba22740f2b5f10804909cf34465546639a14
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-03 19:59:38 -0700

    Set loggedInUserDocId

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,13 +4,17 @@ import Suggestions from "./suggestions";
 
 export default function Sidebar() {
   const {
-    user: { fullName, username, userId, following },
+    user: { docId, fullName, username, userId, following },
   } = useUser();
 
   return (
     <div className="p-4">
       <User username={username} fullName={fullName} />
-      <Suggestions userId={userId} following={following} />
+      <Suggestions
+        userId={userId}
+        following={following}
+        loggedInUserDocId={docId}
+      />
     </div>
   );
 }
