e26f18181da9622b28655a4d4eb70a0384976f2c
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-05 11:49:09 -0700

    Get logged in user photos

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,9 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+
 export default function Timeline() {
   //we need to get the logged in user's photos
+  const { photos } = usePhotos();
   // on loading the photos, we neend to use react skeleton
   // if we have photos, render them(create a post component)
   // if the user has no photos, tell them to create some
