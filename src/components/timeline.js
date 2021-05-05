85b9877066261db98fa5472b20072e60e2f65edc
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-05 14:59:55 -0700

    Use diffrent situations to display timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,13 +4,25 @@ import usePhotos from "../hooks/use-photos";
 export default function Timeline() {
   //we need to get the logged in user's photos
   const { photos } = usePhotos();
+
+  console.log("photos", photos);
   // on loading the photos, we neend to use react skeleton
   // if we have photos, render them(create a post component)
   // if the user has no photos, tell them to create some
 
   return (
     <div className="container col-span-2">
-      <p>I am the timeline</p>
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, index) => (
+            <Skeleton key={index} count={1} width={320} height={400} />
+          ))}
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos! </p>
+      )}
     </div>
   );
 }
