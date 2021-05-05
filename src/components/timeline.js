5682aed655b498bb048bd33087eeb1421bdb08da
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-05 15:35:08 -0700

    Clean up Skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -5,7 +5,6 @@ export default function Timeline() {
   //we need to get the logged in user's photos
   const { photos } = usePhotos();
 
-  console.log("photos", photos);
   // on loading the photos, we neend to use react skeleton
   // if we have photos, render them(create a post component)
   // if the user has no photos, tell them to create some
@@ -13,11 +12,7 @@ export default function Timeline() {
   return (
     <div className="container col-span-2">
       {!photos ? (
-        <>
-          {[...new Array(4)].map((_, index) => (
-            <Skeleton key={index} count={1} width={320} height={400} />
-          ))}
-        </>
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : photos?.length > 0 ? (
         photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
       ) : (
