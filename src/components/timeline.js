b59be2ec017f705c1743bce0dd15f9ffa89890e7
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-04 23:22:16 -0700

    Pseudo code timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,9 @@
 export default function Timeline() {
+  //we need to get the logged in user's photos
+  // on loading the photos, we neend to use react skeleton
+  // if we have photos, render them(create a post component)
+  // if the user has no photos, tell them to create some
+
   return (
     <div className="container col-span-2">
       <p>I am the timeline</p>
