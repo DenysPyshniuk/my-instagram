f628dc48f1f60aba780931045397c2eab0cace68
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-06 13:45:16 -0700

    Import and return Image component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,12 +1,15 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
-export default function Post(content) {
+import Image from "./image";
+
+export default function Post({ content }) {
   // header, image, actions(like & comment icons), foter, comments
 
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-8 ">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
     </div>
   );
 }
