192292b21960cd0b1a8016b11fe33bd32c9bd523
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-06 12:38:04 -0700

    Pass a Post component to timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,6 @@
 import Skeleton from "react-loading-skeleton";
 import usePhotos from "../hooks/use-photos";
+import Post from "./post";
 
 export default function Timeline() {
   //we need to get the logged in user's photos
@@ -14,7 +15,7 @@ export default function Timeline() {
       {!photos ? (
         <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
         <p className="text-center text-2xl">Follow people to see photos! </p>
       )}
