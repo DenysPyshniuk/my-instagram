7c8644b4a55b251b806d9f9cdb1946b36e8583b5
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-07 13:33:05 -0700

    Clear comment input after posting a comment

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -20,6 +20,7 @@ export default function AddComment({
 
     // Give a new array [], put the new comment in the array, add the old comments too
     setComments([{ displayName, comment }, ...comments]);
+    setComment("");
 
     return null;
   };
