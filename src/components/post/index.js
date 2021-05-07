1ff9594899c5744180fc724be5314947a59a85d3
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-07 10:48:58 -0700

    Import comments to the post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -4,6 +4,7 @@ import Header from "./header";
 import Image from "./image";
 import Actions from "./actions";
 import Footer from "./footer";
+import Comments from "./comments";
 
 export default function Post({ content }) {
   // header, image, actions(like & comment icons), foter, comments
@@ -22,6 +23,12 @@ export default function Post({ content }) {
         handleFocus={handleFocus}
       />
       <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 }
