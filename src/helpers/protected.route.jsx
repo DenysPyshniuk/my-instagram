3bdd26e5335be40b1b3676731263373d2d2b7981
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-08 23:00:24 -0700

    Fix a syntax error

--- src/helpers/protected.route.jsx
+++ src/helpers/protected.route.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
-import { Route, Redirect } from "react-route-dom";
-import * as Routes from "../constants/routes";
+import { Route, Redirect } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
 
 export default function ProtectedRoute({ user, children, ...rest }) {
   return (
