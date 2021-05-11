1f6fa35f518fb9442dda536859647123383ed7de
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-10 22:21:41 -0700

    Get user array from database

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,8 +1,33 @@
 import { useParams, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUserByUsername } from "../services/firebase";
+import * as ROUTES from "../constants/routes.js";
 
 export default function Profile() {
   const { username } = useParams();
-  console.log("username", username);
+  const [user, setUser] = useState(null);
+  const [userExists, setUserExists] = useState(false);
+  const history = useHistory();
 
-  return <p>Hello</p>;
+  useEffect(() => {
+    async function checkUserExists() {
+      const user = await getUserByUsername(username);
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExists(true);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+
+    console.log("user", user);
+  }, [username, history]);
+
+  return userExists ? (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">{user.fullName}</div>
+    </div>
+  ) : null;
 }
