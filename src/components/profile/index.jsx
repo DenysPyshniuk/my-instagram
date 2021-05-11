6d3908a9dca2ea5c90a2c967e81e9883c5425be6
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-11 10:26:41 -0700

    Start working on the profile index page

---
+++ src/components/profile/index.jsx
@@ -0,0 +1,17 @@
+import { useReducer, useEffect } from "react";
+import PropTypes from "prop-types";
+import Header from "./header";
+
+const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+  profile: {},
+  photosCollection: [],
+  followerCount: 0,
+};
+
+export default function Profile({ username }) {
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+}
