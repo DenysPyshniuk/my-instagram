f97b919fa28aa6212afc1e59479f2147ed7af97b
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-12 14:51:19 -0700

    Get photos for user

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,6 +1,10 @@
 import { useReducer, useEffect } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
+import {
+  getUserByUsername,
+  getUserPhotosByUsername,
+} from "../../services/firebase";
 
 const reducer = (state, newState) => ({ ...state, ...newState });
 const initialState = {
@@ -14,4 +18,17 @@ export default function Profile({ username }) {
     reducer,
     initialState
   );
+
+  useEffect(() => {
+    async function getProfileInfoAndPhotos() {
+      const [{ ...user }] = await getUserByUsername(username);
+      const photos = getUserPhotosByUsername(username);
+      dispatch({
+        profile: user,
+        photosCollection: photos,
+        followerCount: user.followers.length,
+      });
+    }
+    getProfileInfoAndPhotos();
+  }, []);
 }
