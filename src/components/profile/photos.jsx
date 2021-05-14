a5f0c01dec4a7bf172b00e8ec17020648cc718ac
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-14 14:47:18 -0700

    Add message if no posts yet

--- src/components/profile/photos.jsx
+++ src/components/profile/photos.jsx
@@ -15,7 +15,7 @@ export default function Photos({ photos }) {
             <div key={photos.docId} className="relative group">
               <img src={photo.imageSrc} alt={photo.caption} />
 
-              <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
+              <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex hidden">
                 <p className="flex items-center text-white font-bold">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -51,6 +51,11 @@ export default function Photos({ photos }) {
           ))
         ) : null}
       </div>
+
+      {!photos ||
+        (photos.length === 0 && (
+          <p className="text-center text-2xl"> No Posts Yet</p>
+        ))}
     </div>
   );
 }
