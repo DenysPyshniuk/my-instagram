af31956c7a15b4f38e5950f85c9f4f8081103078
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-14 14:26:18 -0700

    Show pictures on the profile page

--- src/components/profile/photos.jsx
+++ src/components/profile/photos.jsx
@@ -2,7 +2,24 @@ import PropTypes from "prop-types";
 import Skeleton from "react-loading-skeleton";
 
 export default function Photos({ photos }) {
-  return null;
+  console.log(`photos`, photos);
+  return (
+    <div className="h-16 border-t border-gray-primary mt-12 pt-4">
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {!photos ? (
+          <>
+            <Skeleton count={12} width={320} height={400} />
+          </>
+        ) : photos.length > 0 ? (
+          photos.map((photo) => (
+            <div key={photos.docId} className="relative group">
+              <img src={photo.imageSrc} alt={photo.caption} />
+            </div>
+          ))
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 Photos.propTypes = {
