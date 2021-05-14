3e893165752eeb48cee45413142460a1c6c4862e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-14 14:30:51 -0700

    Show the likes for each photo

--- src/components/profile/photos.jsx
+++ src/components/profile/photos.jsx
@@ -16,7 +16,21 @@ export default function Photos({ photos }) {
               <img src={photo.imageSrc} alt={photo.caption} />
 
               <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
-                <p className="flex items-center text-white font-bold"></p>
+                <p className="flex items-center text-white font-bold">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-8 mr-4"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  {photo.likes.length}
+                </p>
               </div>
             </div>
           ))
