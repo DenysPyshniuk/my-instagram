1add1ed9926a3a018b82053bf13fbdacdbfb8e0e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-14 11:02:56 -0700

    Check if user is following profile

--- src/components/profile/header.jsx
+++ src/components/profile/header.jsx
@@ -1,14 +1,39 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Skeleton from "react-loading-skeleton";
+import useUser from "../../hooks/use-user";
+import { isUserFollowingProfile } from "../../services/firebase";
 
 export default function Header({
   photosCount,
-  profile,
   followerCount,
   setFollowerCount,
+  profile: {
+    docId: profileDocId,
+    userId: profileUserId,
+    fullName,
+    following = [],
+  },
 }) {
+  // Information about user who's viewing the page
+  const { user } = useUser();
+
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+
+  useEffect(() => {
+    const isLoggedInUserFollowingProfile = async () => {
+      const isFollowing = await isUserFollowingProfile(
+        user.username,
+        profileUserId
+      );
+      setIsFollowingProfile(isFollowing);
+    };
+
+    if (user.username && profileUserId) {
+      isLoggedInUserFollowingProfile();
+    }
+  }, [user.username, profileUserId]);
+
   return null;
 }
 
