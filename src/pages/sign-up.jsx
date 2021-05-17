cab6bba633e6bda81b625caea5bc761fcf106748
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-16 22:04:11 -0700

    Fix syntax

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -86,7 +86,7 @@ export default function SignUp() {
             <input
               aria-label="Enter your full name"
               type="text"
-              placeholder="Full Name"
+              placeholder="Full name"
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setFullName(target.value)}
               value={fullName}
