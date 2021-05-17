2454cbd0e19715f1860640091fb1c278a5d36fc6
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-16 22:10:18 -0700

    Fix Syntax misspell

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
 
   return (
     <div className="app bg-gray-background">
-      <div className="mx-auth max-w-screen-lg">
+      <div className="mx-auto max-w-screen-lg">
         <p className="text-center text-2xl">Not Found!</p>
       </div>
     </div>
