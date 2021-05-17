8c5847b8acebd4c39b882e05f13e99aca08f0fb7
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-16 22:30:01 -0700

    Pass user

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,10 +1,12 @@
+import { useContext } from "react";
 import Skeleton from "react-loading-skeleton";
 import usePhotos from "../hooks/use-photos";
 import Post from "./post";
 
 export default function Timeline() {
+  const { user } = useContext(LoggedInUserContext);
   //we need to get the logged in user's photos
-  const { photos } = usePhotos();
+  const { photos } = usePhotos(user);
 
   // on loading the photos, we neend to use react skeleton
   // if we have photos, render them(create a post component)
@@ -14,10 +16,8 @@ export default function Timeline() {
     <div className="container col-span-2">
       {!photos ? (
         <Skeleton count={4} width={640} height={500} className="mb-5" />
-      ) : photos?.length > 0 ? (
-        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
-        <p className="text-center text-2xl">Follow people to see photos! </p>
+        photos.map((content) => <Post key={content.docId} content={content} />)
       )}
     </div>
   );
