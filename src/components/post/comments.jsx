8b860f9fb965b30267abc00d8d50038477b20975
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-05-19 09:01:58 -0700

    Show comments newer first

--- src/components/post/comments.jsx
+++ src/components/post/comments.jsx
@@ -19,14 +19,17 @@ export default function Comments({
             View all comments
           </p>
         )}
-        {comments.slice(0, 3).map((item) => (
-          <p key={`${item.comment}-${item.displayName}`} className="mb-1 ">
-            <Link to={`/p/${item.displayName}`}>
-              <span className="mr-1 font-bold">{item.displayName}</span>
-            </Link>
-            <span>{item.comment}</span>
-          </p>
-        ))}
+        {comments
+          .reverse()
+          .slice(0, 3)
+          .map((item) => (
+            <p key={`${item.comment}-${item.displayName}`} className="mb-1 ">
+              <Link to={`/p/${item.displayName}`}>
+                <span className="mr-1 font-bold">{item.displayName}</span>
+              </Link>
+              <span>{item.comment}</span>
+            </p>
+          ))}
         <p className="text-gray-base uppercase text-xs mt-2">
           {formatDistance(posted, new Date())} ago
         </p>
